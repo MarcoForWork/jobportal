d5a6011bfb59b796f09a78bd75e94361079ff6cf
ChisDong <dongminhchi9120@gmail.com>
2025-06-16 21:41:25 +0700

    update sql

--- job_portal.sql
+++ job_portal.sql
@@ -3,35 +3,44 @@ CREATE DATABASE job_portal;
 USE job_portal;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id VARCHAR(255) AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,  
     email VARCHAR(255) UNIQUE NOT NULL,
-    phone VARCHAR(20),  
+    phone VARCHAR(255),  
     role ENUM('candidate', 'recruiter') NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE candidates (
-    id INT PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     dob DATE,
-    address TEXT,
+    address VARCHAR(255),
     cv_file_path VARCHAR(255),
     FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE candidates_skills (
+	id VARCHAR(255),
+	candidate_id VARCHAR(255),
+	experience_level VARCHAR(255),
+	years_of_experience INT,
+	note VARCHAR(255),
+	FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+)
+
 CREATE TABLE recruiters (
-    id INT PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     company_name VARCHAR(255),
     FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE job_posts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    recruiter_id INT NOT NULL,
+    id VARCHAR(255) AUTO_INCREMENT PRIMARY KEY,
+    recruiter_id VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
-    description TEXT,
+    description VARCHAR(255),
     location VARCHAR(255),
     salary VARCHAR(100),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -40,9 +49,9 @@ CREATE TABLE job_posts (
 );
 
 CREATE TABLE applications (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    candidate_id INT NOT NULL,
-    job_post_id INT NOT NULL,
+    id VARCHAR(255) AUTO_INCREMENT PRIMARY KEY,
+    candidate_id VARCHAR(255) NOT NULL,
+    job_post_id VARCHAR(255) NOT NULL,
     applied_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     status ENUM('applied', 'reviewed', 'accepted', 'rejected') DEFAULT 'applied',
     FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE,
