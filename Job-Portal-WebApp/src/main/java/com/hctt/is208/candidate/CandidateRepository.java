e2969d27d3dd6352a0d3de6f5a09f77b04b0a044
Marco <hoang.le2160.4work@gmail.com>
2025-06-16 21:29:56 +0700

    refactor

--- Job-Portal-WebApp/src/main/java/com/hctt/is208/candidate/CandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hctt.is208.candidate;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CandidateRepository extends JpaRepository<candidates, Integer> {
-}
